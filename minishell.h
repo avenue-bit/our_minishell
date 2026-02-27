ebb093b5f2f6fb02ea5d083bbddd8e138a2f6ae6
Elliot Sezalory <esezalor@c1r1p6.42vienna.com>
2026-02-27 16:02:05 +0100

    Updated libft_utils

--- minishell.h
+++ minishell.h
@@ -2,6 +2,7 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+//# include "libft_utils/libft_utils.h"
 # include <errno.h>
 # include <fcntl.h>
 # include <stdio.h>
@@ -12,9 +13,11 @@
 # ifndef BOOL
 #  define BOOL _Bool
 # endif
+
 # ifndef FALSE
 #  define FALSE 0
 # endif
+
 # ifndef TRUE
 #  define TRUE 1
 # endif
@@ -48,5 +51,43 @@ typedef struct s_cmd
 	struct s_cmd	*prev;
 
 }					t_cmd;
+
+typedef struct s_env
+{
+	char			*key;
+	char			*content;
+	struct s_env	*next;
+}					t_env;
+
+typedef struct s_shell
+{
+	struct s_env	*environment;
+	char			**execve_env;
+	int				n_env_variables;
+	char			**all_paths;
+	int				n_paths;
+	char			*command_path;
+}					t_shell;
+
+// Environment Initialisation
+char				*fetch_key(char *environment);
+char				*fetch_content(char *environment);
+t_env				*envnodes_init(char **envp);
+char				**envarray_init(t_shell *storage, t_env *environments);
+char				*env_join(char *key, char *content);
+
+// Path Initialisation
+int					extract_path(t_shell *shell_storage);
+int					check_absolute(char *command);
+char				*pathfinder(t_shell *storage, char *command);
+int					path_ramp(t_shell *storage, char **argv);
+
+// Libft Utils
+void				ft_arrayfree(char **str_array, int n);
+int					ft_envsize(t_env *lst);
+
+// Environment Utils
+void				env_clearnode(t_env **env_lst);
+t_env				*env_newnode(char *environment);
 
 #endif
