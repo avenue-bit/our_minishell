5628759313b5f68e811b879ab0709446beaf3e49
avenue-bit <f42vienna@gmail.com>
2026-03-16 18:32:05 +0100

    Merging lexing-branch to the main (#4)
    * Import minishell.h from headerfiles branch
    
    * creating the cmd list and fill the data to the cmd list
    
    * updating the count_tokens_words function and adding a print of the cmd list function for debugging purposes
    
    * protecting for malloc fails in the functions fill_cmd_data, create_cmd_list
    
    * clearing mallocs for the Error of a missing " or \'
    
    * adding clean exits after malloc fails
    
    * rewriting fill_cmd_data_redir() do handle also multiple redirections
    
    * rewriting fill_cmd_data_redir() do handle also multiple redirections
    
    * handling the error cascade in from fill_data_redir to create_cmd_list
    
    * improving all malloc failure error handlings
    
    * improving create_tokens to check only for the spaces (9 to 13) and not 0 to 13
    
    * started to make improvements for using the readline loop
    
    * changing when ENOENT to continue not to kill process
    
    * headerfile update
    
    * removing the readme file .. for now
    
    ---------

--- minishell.h
+++ minishell.h
@@ -2,8 +2,10 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+//# include "libft_utils/libft_utils.h"
 # include <errno.h>
 # include <fcntl.h>
+# include <readline/history.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <sys/wait.h>
@@ -12,9 +14,11 @@
 # ifndef BOOL
 #  define BOOL _Bool
 # endif
+
 # ifndef FALSE
 #  define FALSE 0
 # endif
+
 # ifndef TRUE
 #  define TRUE 1
 # endif
@@ -56,6 +60,7 @@ typedef struct s_env
 	struct s_env	*next;
 }					t_env;
 
+<<<<<<< HEAD
 typedef struct s_shell
 {
 	struct s_env	*environment;
@@ -63,11 +68,43 @@ typedef struct s_shell
 	char			**all_paths;
 	char			*command_path;
 }					t_shell;
+=======
+//typedef int			(*builtin_ptr)(t_exec *, t_cmd *);
+
+typedef struct s_exec
+{
+	struct s_env	*environment;
+	char			**execve_env;
+	int				n_env_variables;
+	char			**all_paths;
+	int				n_paths;
+	char			*command_path;
+	char			*builtins[8];
+	//builtin_ptr		*builtin_func[8];
+	int				pre_read_fd;
+	int				pipe_fd[2];
+	int				infile_fd;
+	int				outfile_fd;
+	int				exit_code;
+	int				last_pid;
+}					t_exec;
+
+//********************************************************//
+// THIS IS FOR TESTING PURPOSES WHILE JOSH FINISHES WORK
+//********************************************************//
+t_cmd				*cmdnodes_init(char **argv);
+t_cmd				*cmd_newnode(char **command);
+void				cmd_clearnode(t_cmd **cmd_lst);
+//********************************************************//
+// THIS IS FOR TESTING PURPOSES WHILE JOSH FINISHES WORK
+//********************************************************//
+>>>>>>> origin/lexing
 
 // Environment Initialisation
 char				*fetch_key(char *environment);
 char				*fetch_content(char *environment);
 t_env				*envnodes_init(char **envp);
+<<<<<<< HEAD
 char				**envarray_init(t_env *environments);
 char				*env_fullenv(char *key, char *content);
 
@@ -80,9 +117,57 @@ int					path_ramp(t_shell *storage, char **argv);
 // Libft Utils
 void				ft_arrayfree(char **str_array, int n);
 int					ft_envsize(t_env *lst);
+=======
+char				**envarray_init(t_exec *storage, t_env *environments);
+char				*env_join(char *key, char *content);
+
+// Path Initialisation
+int					extract_path(t_exec *shell_storage);
+int					check_absolute(char *command);
+char				*pathfinder(t_exec *storage, char *command);
+int					path_ramp(t_exec *storage, char **argv);
+
+// Pipe and Fork Functions
+int					fork_ramp(t_exec *storage, t_cmd *cmd_node);
+void				exec_fork(t_exec *storage, t_cmd *cmd_node);
+void				child_wrapper(t_exec *storage, t_cmd *current);
+void				parent_wrapper(t_exec *storage, t_cmd *current);
+void				wait_for_child(t_exec *storage);
+
+// Redirection Functions
+void				infile_outfile_check(t_exec *storage, t_cmd *cmd_node);
+void				open_infile(t_exec *storage, t_cmd *cmd_node);
+void				open_outfile(t_exec *storage, t_cmd *cmd_node);
+
+// Built-In Functions
+int					is_builtin(t_exec *storage, char *command);
+int					exec_builtin(t_exec *storage, t_cmd *cmd_node);
+int					ft_echo(t_exec *storage, t_cmd cmd_node);
+int					ft_cd(t_exec *storage, t_cmd cmd_node);
+int					ft_pwd(t_exec *storage, t_cmd cmd_node);
+int					ft_export(t_exec *storage, t_cmd cmd_node);
+int					ft_unset(t_exec *storage, t_cmd cmd_node);
+int					ft_env(t_exec *storage, t_cmd cmd_node);
+int					ft_exit(t_exec *storage, t_cmd cmd_node);
+
+// Free and Close Functions
+void				path_env_free(t_exec *storage);
+void				failexec_close(t_exec *storage);
+void				heredoc_cleanup(t_cmd *head_node);
+>>>>>>> origin/lexing
 
 // Environment Utils
 void				env_clearnode(t_env **env_lst);
 t_env				*env_newnode(char *environment);
 
+<<<<<<< HEAD
 #endif
+=======
+// AdHoc Utils
+void				ft_arrayfree(char **str_array, int n);
+int					ft_envsize(t_env *lst);
+size_t				n_commands(t_cmd *cmd);
+
+#endif
+
+>>>>>>> origin/lexing
