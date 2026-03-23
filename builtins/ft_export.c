e2fa842dc514c7185353c238936d9e072568c173
Elliot Sezalory <esezalor@c3r4p5.42vienna.com>
2026-03-23 21:31:21 +0100

    ft_export, only missin the sorting, adding node and printing

--- builtins/ft_export.c
+++ builtins/ft_export.c
@@ -6,13 +6,113 @@
 /*   By: esezalor <esezalor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/11 16:57:46 by esezalor          #+#    #+#             */
-/*   Updated: 2026/03/23 15:51:57 by esezalor         ###   ########.fr       */
+/*   Updated: 2026/03/23 21:30:51 by esezalor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int						ft_export(t_exec *storage, t_cmd *cmd_node)
+// Return Values:
+// 0: found/updated
+// 1: not found
+// -1: allocation error
+
+int	ft_export(t_exec *storage, t_cmd *cmd_node)
 {
-	
+	int	i;
+	int	valid_key;
+	int	exit_status;
+	int	env_changed;
+
+	env_changed = 0;
+	exit_status = 0;
+	i = 1;
+	if (!cmd_node->cmd_flags[i])
+		declare_x(); // not built yet,will do this at the end along with sorting
+	while (cmd_node->cmd_flags[i])
+	{
+		valid_key = valid_export_key(cmd_node->cmd_flags[i]);
+		if (valid_key >= 0 && export_path(storage, cmd_node->cmd_flags[i],
+				valid_key, &env_changed))
+			return (1);
+		if (valid_key < 0)
+			exit_status = export_error(cmd_node->cmd_flags[i]);
+		i++;
+	}
+	if (env_changed && update_execve_env(storage))
+		return (1);
+	return (exit_status);
+}
+
+int	export_path(t_exec *storage, char *export_var, int valid_key, int *env_chg)
+{
+	int	valid_path;
+
+	valid_path = findnedit(storage, export_var, valid_key);
+	if (valid_path == -1)
+		return (1);
+	if (valid_path == 1)
+		*env_chg = add_path(storage, export_var); // to build
+	else if (valid_path == 0)
+		*env_chg = 1;
+	return (0);
+}
+
+int	valid_export_key(char *export_var)
+{
+	int	i;
+
+	i = 0;
+	if (!ft_isalpha(export_var[i]) && export_var[i] != '_')
+		return (-1);
+	while (export_var[i] && export_var[i] != '=')
+	{
+		if (export_var[i] == '+' && export_var[i + 1] == '=')
+			return (1);
+		if (!ft_isalpha(export_var[i]) && !ft_isdigit(export_var[i])
+			&& export_var[i] != '_')
+			return (-1);
+		i++;
+	}
+	return (0);
+}
+
+int	findnedit(t_exec *storage, char *export_var, int flag)
+{
+	t_env	*current;
+	char	*nkey;
+	int		nkey_len;
+
+	current = storage->environment;
+	nkey = fetch_key(export_var);
+	if (!nkey)
+		return (-1);
+	nkey_len = ft_strlen(nkey) + 1;
+	while (current)
+	{
+		if (ft_strncmp(current->key, nkey, nkey_len) == 0)
+			return (free(nkey), replace_or_append(current, export_var, flag));
+		current = current->next;
+	}
+	return (free(nkey), 1);
+}
+
+int	replace_or_append(t_env *current, char *export_var, int flag)
+{
+	if (flag == 0)
+	{
+		if (find_char(export_var, '='))
+		{
+			free(current->content);
+			current->content = fetch_content(export_var);
+			if (!current->content)
+				return (-1);
+		}
+	}
+	else
+	{
+		if (append_export_content(current, export_var) == -1)
+			return (-1);
+	}
+	return (0);
 }
--- builtins/ft_export_utils.c
+++ builtins/ft_export_utils.c
@@ -6,13 +6,45 @@
 /*   By: esezalor <esezalor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/11 16:57:46 by esezalor          #+#    #+#             */
-/*   Updated: 2026/03/23 15:52:20 by esezalor         ###   ########.fr       */
+/*   Updated: 2026/03/23 21:30:42 by esezalor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int						ft_export(t_exec *storage, t_cmd *cmd_node)
+int	export_error(char *export_var)
 {
-	return(0);
+	ft_putstr_fd("bash: export: \'", 2);
+	ft_putstr_fd(export_var, 2);
+	ft_putstr_fd("\': not a valid identifier\n", 2);
+	return (1);
+}
+
+int	append_export_content(t_env *current, char *export_var)
+{
+	char	*export_content;
+	char	*new_content;
+
+	export_content = fetch_content(export_var);
+	if (!export_content)
+		return (-1);
+	if (!current->content)
+		return(current->content = export_content, 0);
+	else
+		new_content = ft_strjoin(current->content, export_content);
+	if (!new_content)
+		return (free(export_content), -1);
+	free(current->content);
+	current->content = new_content;
+	free(export_content);
+	return (0);
+}
+
+int update_execve_env(t_exec *storage)
+{
+    ft_arrayfree(storage->execve_env);
+    storage->execve_env = envarray_init(storage, storage->environment);
+    if (!storage->execve_env)
+        return (1);
+    return (0);
 }
